bd8e015bf4c517a16580a670773a8cbfbc7569b6
[Yashwanth Banda] <bandayashwanth@gmail.com>
2018-12-02 20:25:18 -0600

    updated war file

---
+++ target/classes/templates/main.js
@@ -0,0 +1,16 @@
+
+var multipleFileUploadSuccess = document.querySelector('#multipleFileUploadSuccess');
+
+
+
+function downloadMultipleFiles(files) {
+   
+        console.log(xhr.responseText);
+              multipleFileUploadError.style.display = "none";
+                content += "<p>DownloadUrl : <a href='" + file + "' target='_blank'>" + response[i].fileDownloadUri + "</a></p>";
+            multipleFileUploadSuccess.innerHTML = content;
+            multipleFileUploadSuccess.style.display = "block";
+ 
+    }
+
+}
